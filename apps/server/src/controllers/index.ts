279d35d5b0fb743c733d9afbc1ab30946071dd8d
ximing <ximing@meituan.com>
2026-02-15 09:04:11 +0800

    refactor: 添加 StaticController 到控制器列表并调整加载顺序

--- apps/server/src/controllers/index.ts
+++ apps/server/src/controllers/index.ts
@@ -2,7 +2,7 @@ import { MemoV1Controller } from './v1/memo.controller.js';
 import { AuthV1Controller } from './v1/auth.controller.js';
 import { UserV1Controller } from './v1/user.controller.js';
 import { CategoryV1Controller } from './v1/category.controller.js';
-import { AttachmentV1Controller, AttachmentFileController } from './v1/attachment.controller.js';
+import { AttachmentV1Controller } from './v1/attachment.controller.js';
 import { StaticController } from './static.controller.js';
 
 // Note: StaticController should be last to handle all non-API routes
@@ -12,6 +12,5 @@ export const controllers = [
   UserV1Controller,
   CategoryV1Controller,
   AttachmentV1Controller,
-  AttachmentFileController,
   StaticController,
 ];
