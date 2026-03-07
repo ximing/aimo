f58340b96dc2ac979cdf10facc6c78a5522dec92
ximing <ximing@meituan.com>
2026-03-07 20:55:58 +0800

    feat: 优化客户端

---
+++ apps/client/src/main/ipc-handlers.ts
@@ -0,0 +1,27 @@
+import { app, ipcMain } from 'electron';
+
+import { checkForUpdates, downloadUpdate, installUpdate } from './updater';
+
+export function registerIpcHandlers(): void {
+  ipcMain.handle('log-preload', (_event, data) => {
+    console.log('[Preload] Debug info:', data);
+    return { success: true };
+  });
+
+  ipcMain.handle('check-for-updates', async () => {
+    return await checkForUpdates();
+  });
+
+  ipcMain.handle('download-update', async () => {
+    await downloadUpdate();
+    return { success: true };
+  });
+
+  ipcMain.handle('install-update', () => {
+    installUpdate();
+  });
+
+  ipcMain.handle('get-app-version', () => {
+    return app.getVersion();
+  });
+}
